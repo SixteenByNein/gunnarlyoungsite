5f27fea76a2e3be1f70f352a73132bb483122200
SixteenByNein <gunnarlorenzoyoung@gmail.com>
2025-04-20 20:15:54 -0600

    Migrated to Liger

---
+++ layout/projects/cowAI/script.js
@@ -0,0 +1,32 @@
+var chat_log = "";
+
+function main(event)
+{
+
+    if(event.key =="Enter"){
+
+    var prompt = document.getElementById("prompt").value;
+
+    document.getElementById("prompt").value = "";
+
+    var output = document.getElementById("chat_history");
+
+    var answer = "M";
+
+    var answer_len = Math.floor(Math.random() * 9 + 2);
+
+    for (let i = 0; i < answer_len; i++){
+
+        answer = answer + "o";
+
+    }
+
+
+    chat_log = chat_log + "You:     " + prompt + "<br>" + "BovAi:       " + answer + "<br>";
+
+    output.innerHTML = chat_log;
+
+    }
+
+
+}
