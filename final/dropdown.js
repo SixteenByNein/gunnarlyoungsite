17a48efb852a2b3c0bcf5c834119b26c56d6f6b2
SixteenByNein <gunnarlorenzoyoung@gmail.com>
2025-04-28 10:50:24 -0600

    cleaned up dropdown menu

--- final/dropdown.js
+++ final/dropdown.js
@@ -3,18 +3,26 @@ function toggleMenu()
 
 
     var menu = document.getElementById("collapsed");
-    var bottom = document.getElementById("bottomShelf");
+
+    var overlay = document.getElementById("mobile");
 
     if(menu.style.display == "flex")
     {
     menu.style.display="none";
-    bottom.style.position="fixed";
+
+    overlay.style.backdropFilter="none";
+
+    overlay.style.backgroundColor="rgba(0,0,0,0)";
     }
 
     else
     {
     menu.style.display="flex";
-    bottom.style.position="relative";
+
+    overlay.style.backdropFilter="blur(4px)";
+
+    overlay.style.backgroundColor="rgba(23, 4, 53, 0.603)";
+
     }
 
 }
