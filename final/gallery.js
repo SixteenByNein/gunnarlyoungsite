cdb79158e0067200d501745d529a90d3a94d3231
SixteenByNein <gunnarlorenzoyoung@gmail.com>
2025-05-20 14:19:35 -0600

    Updated Doodle

---
+++ final/gallery.js
@@ -0,0 +1,17 @@
+function openPreview(name)
+{
+
+    let overlay = document.getElementById("overlay");
+
+    overlay.style.display="flex";
+
+}
+
+function closePreview()
+{
+
+    let overlay = document.getElementById("overlay");
+
+    overlay.style.display="none";
+
+}
