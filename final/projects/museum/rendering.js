0f07837da16ef83b8be7e3932c3a420d2cd5ce3c
SixteenByNein <gunnarlorenzoyoung@gmail.com>
2025-07-24 22:56:18 -0600

    Changed Logo

---
+++ final/projects/museum/rendering.js
@@ -0,0 +1,10 @@
+function render() {
+
+    const canvas = document.getElementById("canvas");
+    if (canvas.getContext) {
+          const ctx = canvas.getContext("2d");
+    }
+
+}
+
+window.addEventListener("load", render);
